371783b6dbde3e0cec1a8663c6d0da5bba349b6a
Colton Steele <colton.steele@ou.edu>
2020-01-25 13:52:16 -0600

    Finished receipt, done

--- Homework 1/Program.cs
+++ Homework 1/Program.cs
@@ -16,6 +16,7 @@ namespace Homework_1
             const double lowmarkup = 1.125;
             double csaleprice = 0;
             double gsaleprice = 0;
+            double discount;
 
             Console.WriteLine("How many cogs are being purchased?");
             double answer = Convert.ToDouble(Console.ReadLine());
@@ -26,13 +27,17 @@ namespace Homework_1
             if(answer>=10 || answer2>=10 || answer+answer2>=16)
             {
                 csaleprice = cogs * lowmarkup;
-                gsaleprice = cogs * lowmarkup;
+                gsaleprice = gears * lowmarkup;
+                discount = (cogs * standmarkup * answer + gears * standmarkup * answer2) - (cogs * lowmarkup * answer + gears * lowmarkup * answer2);
             }
             else
             {
                 csaleprice = cogs * standmarkup;
-                gsaleprice = cogs * standmarkup;
+                gsaleprice = gears * standmarkup;
+                discount = 0;
             }
+
+            
             double total;
             double subtotal;
             double tax=0.089;
@@ -44,13 +49,21 @@ namespace Homework_1
             gextension = gsaleprice * answer2;
             subtotal = cextension + gextension;
 
+            
+
             taxtotal = subtotal * tax;
             total = subtotal + taxtotal;
             
             Console.WriteLine("\n---------------------------------------------\n");
             Console.WriteLine("              RECEIPT         ");
-            Console.WriteLine(answer+" Cogs @  $"+csaleprice+"-------->$"+cextension);
-            Console.WriteLine(answer2 + " Gears @  $" + gsaleprice + "-------->$" + gextension);
+            Console.WriteLine($"{answer} Cogs @ ${csaleprice:N2} ---------> ${cextension:N2}");
+            Console.WriteLine($"{answer2} Gears @ ${gsaleprice:N2} ---------> ${gextension:N2}");
+
+            Console.WriteLine($"                      Discount: ${discount:N2}");
+            Console.WriteLine($"                      Subtotal: ${subtotal:N2}");
+            Console.WriteLine($"                      Tax @ 8.9%: ${taxtotal:N2}");
+            Console.WriteLine("                      ----------------------------");
+            Console.WriteLine($"                      Total: ${total:N2}");
             Console.ReadKey();
         }
 
