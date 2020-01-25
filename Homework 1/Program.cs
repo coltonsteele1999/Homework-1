7fd2a0b50386351f68c1fa3c0f538b4473494c87
Colton Steele <colton.steele@ou.edu>
2020-01-25 13:08:22 -0600

    Connected github, declared 4 variables, two console writelines, first if statement

---
+++ Homework 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cogs = 79.99;
+            double gears = 250.00;
+            double csaleprice = 0;
+            double gsaleprice = 0;
+
+            Console.WriteLine("How many cogs are being purchased?");
+            double answer = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("How many gears are being purchased?");
+            double answer2 = Convert.ToDouble(Console.ReadLine());
+
+            if(answer>=10 || answer2>=10 || answer+answer2>=16)
+            {
+
+            }
+        }
+    }
+}
